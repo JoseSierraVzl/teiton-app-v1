dbeed12303258d60ff52441c72b96768416ba760
jasm <sierramendezjosealejandro@gmail.com>
2024-05-29 14:13:59 -0400

    Integration libraries

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
-import './assets/main.css'
+import './assets/sass/tailwind.sass';
+import './assets/sass/main.sass';
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import vuetify from './plugins/vuetify'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
+app.use(vuetify)
 app.use(createPinia())
 app.use(router)
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,13 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h1 class="text-9xl">{{ message }}</h1>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Hello Alejandro'
+    }
+  }
+}
+</script>
